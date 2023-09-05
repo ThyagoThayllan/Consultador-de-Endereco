929ad642a367c5a76dfa98d28d01bf771e1ddd6c
Thyago Thayllan <thayllanst@outlook.com>
2023-09-05 20:25:30 -0300

    atualizações

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import styles from './App.module.css'
 import axios from 'axios'
 import mapIcon from './imgs/map-icon.svg'
